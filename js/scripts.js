71de3c4d495f527f90fe10fb558869f4257da6a2
Lucian <lucian.miller185@gmail.com>
2021-07-08 09:38:26 -0700

    It will fix typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
 
 //User Interface Logic-------
-$(document).ready(fucntion() {
-  $("form#ticket").submit(function(event) {
+$(document).ready(function() {
+  $("form#tickets").submit(function(event) {
     event.preventDefault();
     const selectedMovie = parseInt($("#movie").val());
     const selectedTime = $("#time").val();
-    const selectedAge = parseInt($("#age").val();)
+    const selectedAge = parseInt($("#age").val());
   });
 });
