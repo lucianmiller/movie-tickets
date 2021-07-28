255d1ba047e68ac1d022561349ec9a7c2f136156
Lucian <lucian.miller185@gmail.com>
2021-07-28 10:52:13 -0700

    It will calculate total price of all the tickets

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 //Business Logic for TicketBooth --------
 function TicketBooth() {
-  this.ticket = {}
+  this.tickets = []
   this.currentId = 0
+  this.checkoutPrice = 0
 }
 
 TicketBooth.prototype.addTicket = function(ticket) {
   ticket.id = this.assignId();
-  this.tickets[ticket.id] = ticket;
+  this.tickets.push(ticket);
 }
 
 TicketBooth.prototype.assignId = function() {
@@ -21,18 +22,22 @@ TicketBooth.prototype.findTicket = function(id) {
   return false;
 }
 
-// Business Logic for Ticket --------
-
-
-function Ticket(movie, time, age) {
-  this.movie = movie;
-  this.time = time;
-  this.age = age;
-  this.price = 0;
+TicketBooth.prototype.totalPrice = function() {
+  for (let i = 0; i < this.tickets.length; i++) {
+    this.checkoutPrice += this.tickets[i].price
+  }
 }
 
-
-
+  // Business Logic for Ticket --------
+  
+  
+  function Ticket(movie, time, age) {
+    this.movie = movie;
+    this.time = time;
+    this.age = age;
+    this.price = 0;
+  }
+  
 Ticket.prototype.calculatePrice = function() {
   if (this.movie === 1) {
     this.price += 10;
@@ -56,15 +61,37 @@ Ticket.prototype.calculatePrice = function() {
 //User Interface Logic-------
 let ticketBooth = new TicketBooth;
 
-$(document).ready(function() {
-  $("form#tickets").submit(function(event) {
-    event.preventDefault();
-    const selectedMovie = parseInt($("#movie").val());
-    const selectedTime = parseInt($("#time").val());
-    const selectedAge = parseInt($("#age").val());
+const userInputtedMovie = 1
+const userInputtedMovieTime = 2
+const userInputtedAge = 3
+
+let userTicket = new Ticket(userInputtedMovie, userInputtedMovieTime, userInputtedAge);
+let userTicket2 = new Ticket(userInputtedMovie, userInputtedMovieTime, userInputtedAge);
+userTicket.calculatePrice();
+userTicket2.calculatePrice();
+ticketBooth.addTicket(userTicket);
+ticketBooth.addTicket(userTicket2);
+ticketBooth.totalPrice();
+
+console.log(ticketBooth.checkoutPrice);
+
+
+// $(document).ready(function() {
+//   $("form#tickets").submit(function(event) {
+//     event.preventDefault();
+//     const selectedMovie = parseInt($("#movie").val());
+//     const selectedTime = parseInt($("#time").val());
+//     const selectedAge = parseInt($("#age").val());
     
-    let userTicket = new Ticket(selectedMovie, selectedTime, selectedAge);
-    userTicket.calculatePrice();
-    console.table(userTicket);
-  });
-});
+//     $("input#movie").val("");
+//     $("input#time").val("");
+//     $("input#age").val("");
+
+//     let userTicket = new Ticket(selectedMovie, selectedTime, selectedAge);
+//     userTicket.calculatePrice();
+//     ticketBooth.addTicket(userTicket);
+
+//     console.table(userTicket);
+//     console.table(ticketBooth);
+//   });
+// });
